affe118d316e960ab75e05843932900a23d34695
Chris Calascione <ccalascione@riversagency.com>
2018-04-24 17:09:30 -0400

    add align self option

--- examples/script.js
+++ examples/script.js
@@ -8,33 +8,62 @@
             boxes: {
                 flexBoxOne: {
                     grow: 0,
-                    shrink: 0,
+                    shrink: 1,
                     basis: 'auto',
+                    alignSelf: 'auto'
                 },
                 flexBoxTwo: {
                     grow: 0,
-                    shrink: 0,
+                    shrink: 1,
                     basis: 'auto',
+                    alignSelf: 'auto'
                 },
                 flexBoxThree: {
                     grow: 0,
-                    shrink: 0,
+                    shrink: 1,
                     basis: 'auto',
+                    alignSelf: 'auto'
                 },
                 flexBoxFour: {
                     grow: 0,
-                    shrink: 0,
+                    shrink: 1,
                     basis: 'auto',
+                    alignSelf: 'auto'
                 }
-            }
+            },
+            justifyContent: 'flex-start',
+            alignItems: 'stretch',
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            containerWidth: '100%',
+            containerHeight: 'auto',
         },
         methods: {
-            reset: function () {
+            resetFlexItems: function () {
                 for (let box in this.boxes) {
                     this.boxes[box].grow = 0;
-                    this.boxes[box].shrink = 0;
+                    this.boxes[box].shrink = 1;
                     this.boxes[box].basis = 'auto';
+                    this.boxes[box].alignSelf = 'auto';
                 }
+            },
+
+            resetFlexContainer: function () {
+                this.containerWidth = '100%';
+                this.containerHeight = 'auto';
+            },
+
+            resetFlexContainerFlexProperties: function () {
+                this.justifyContent = 'flex-start';
+                this.alignItems = 'stretch';
+                this.flexDirection = 'row';
+                this.flexWrap = 'nowrap';
+            },
+
+            resetAll: function () {
+                this.resetFlexItems();
+                this.resetFlexContainer();
+                this.resetFlexContainerFlexProperties();
             }
         }
     })
